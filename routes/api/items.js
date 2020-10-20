58b0a75fb9eb291adb510edc7918ee921bf0e0be
Gael Gohoungo <gohoungogael@gmail.com>
2020-10-20 00:54:45 -0400

    redux set up

--- routes/api/items.js
+++ routes/api/items.js
@@ -1,39 +1,67 @@
-const express = require('express');
-const router = express.Router();
-
+import { Router } from 'express';
+import auth from '../../middleware/auth';
 // Item Model
-const Item = require('../../models/Item');
+import Item from '../../models/Item';
 
-// @route GET api/items
-// @desc Get all tems
-// @acces Public
-router.get('/', (req, res) => {
-	Item.find()
-		.sort({ date: -1 })
-		.then((items) => res.json(items));
+const router = Router();
+
+/**
+ * @route   GET api/items
+ * @desc    Get All Items
+ * @access  Public
+ */
+
+router.get('/', async (req, res) => {
+  try {
+    const items = await Item.find();
+    if (!items) throw Error('No items');
+
+    res.status(200).json(items);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
 });
 
-// @route Post api/items
-// @desc Ceate a post
-// @acces Public
-router.post('/', (req, res) => {
-	const newItem = new Item({
-		name: req.body.name,
-	});
+/**
+ * @route   POST api/items
+ * @desc    Create An Item
+ * @access  Private
+ */
 
-	newItem.save().then((item) => res.json(item));
+router.post('/', auth, async (req, res) => {
+  const newItem = new Item({
+    name: req.body.name
+  });
+
+  try {
+    const item = await newItem.save();
+    if (!item) throw Error('Something went wrong saving the item');
+
+    res.status(200).json(item);
+  } catch (e) {
+    res.status(400).json({ msg: e.message });
+  }
 });
 
-// @route DELETE api/items/ :id
-// @desc Ceate a post
-// @acces Public
-router
-	.delete('/:id', (req, res) => {
-		Item
-			.findById(req.params.id)
-            .then(item => item.remove().then(() => res.json({ success: true })))
-            .catch(err => res.status(404).json({ success: false }));
-	});
-	
+/**
+ * @route   DELETE api/items/:id
+ * @desc    Delete A Item
+ * @access  Private
+ */
 
-module.exports = router;
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const item = await Item.findById(req.params.id);
+    if (!item) throw Error('No item found');
+
+    const removed = await item.remove();
+    if (!removed)
+      throw Error('Something went wrong while trying to delete the item');
+
+    res.status(200).json({ success: true });
+  } catch (e) {
+    res.status(400).json({ msg: e.message, success: false });
+  }
+});
+
+export default router;
